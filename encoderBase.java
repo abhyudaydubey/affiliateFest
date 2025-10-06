635e29df67a5063c16b21175bfc6b6f943e7ea45
abhyudaydubey4 <116032708+abhyudaydubey4@users.noreply.github.com>
2025-10-07 00:22:03 +0530

    Update encoderBase.java

--- encoderBase.java
+++ encoderBase.java
@@ -9,6 +9,6 @@ public class Base64Encoder {
         String encodedString = Base64.getEncoder().encodeToString(originalString.getBytes());
         
         System.out.println("Original: " + originalString);
-        System.out.println("Base64 Encoded: " + encodedString);
+        System.out.println("Base64 Encoded: " + encodedString); // encoder not used anywhere
     }
 }
